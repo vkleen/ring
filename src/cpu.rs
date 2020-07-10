d098cd613f4f2027d3c706ddf6c8acaf4cd27fd9
Eric Richter <erichte@linux.ibm.com>
2020-07-10 14:16:23 -0500

    cpu.rs: don't build the arm module when on non-arm
    For initial ppc64le support, we are assuming POWER8 and above, therefore
    we do not (yet?) need a specific Features struct for the architecture.
    However, this arm struct will complain that it is never constructed, and
    there are unused functions. Therefore, we don't build it on non-arm.
    
    I agree to license my contributions to each file under the terms given
    at the top of each file I changed.

--- src/cpu.rs
+++ src/cpu.rs
@@ -60,6 +60,10 @@ pub(crate) fn features() -> Features {
     Features(())
 }
 
+#[cfg_attr(
+    not(any(target_arch = "arm", target_arch = "aarch64")),
+    allow(dead_code)
+)]
 pub(crate) mod arm {
     #[cfg(all(
         any(target_os = "android", target_os = "linux"),
